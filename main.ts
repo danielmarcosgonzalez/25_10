abc74251a601f0ac066145d129681c93ce19df4a
danielmarcosgonzalez <125558887+danielmarcosgonzalez@users.noreply.github.com>
2024-10-25 12:36:22 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+import {MongoClient } from 'mongodb'
+
+const url = Deno.env.get("MONGO_URL");
+if(!url){
+  console.log("not url MongoDB");
+  Deno.exit(-1);
+}
+
+const client = new MongoClient(url);
